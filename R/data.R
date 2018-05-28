ccc345f4f06610330e530a2a3ec9c13f879ce04e
zouter <wouter.saelens@gmail.com>
2018-05-28 12:17:56 +0200

    add data docs

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' Dataset containing trans-differentiating fibroblasts from [Treutlein et. al.(http://doi.org/10.1038/nature18323).
+#'
+#' @source [http://doi.org/10.1038/nature18323 (http://doi.org/10.1038/nature18323)]
+#' @docType data
+"fibroblast_reprogramming_treutlein"
