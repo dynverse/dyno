09a1315294c3b5e4b5fe0f68b75474ea245bc0b0
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-07 14:02:20 +0200

    fix =s

--- generate_site.R
+++ generate_site.R
@@ -16,7 +16,7 @@ reference <- pmap(packages, function(pkg_id, title, desc) {
 
   man_files <- dir_ls(pkg_dir)
 
-  walk(man_files, ~fs::file_copy(., paste0("./man/", fs::path_file(.)), overwrite=TRUE))
+  walk(man_files, ~fs::file_copy(., paste0("./man/", fs::path_file(.)), overwrite = TRUE))
 
   man_ids <- man_files %>% {.[str_which(., ".*\\.Rd")]} %>% fs::path_file() %>% str_extract("[^\\.]*")
 
@@ -28,13 +28,13 @@ reference <- pmap(packages, function(pkg_id, title, desc) {
 })
 
 config <- list(
-  reference=reference,
+  reference = reference,
   development = list(mode = "unreleased")
 )
 
 yaml::write_yaml(config, "_pkgdown.yml")
 
-build_site(".", lazy=TRUE)
+build_site(".", lazy = TRUE)
 
 dir_walk("./man/", fs::file_delete)
 
